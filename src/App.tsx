f33b6cd059ff30b5eab6d1ce1be3941122b35503
MoonJinjin <s2019s11@e-mirim.hs.kr>
2024-04-17 22:48:56 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+
+const App: React.FC = () => {
+	return (
+		<>
+			<div className="App">
+				header
+				main
+				footer
+			</div>
+		</>
+	);
+};
+export default App;
